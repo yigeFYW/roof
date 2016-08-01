66c8ca07e3e2ea779e646499cb7b5d24f11b6f4e
FYW <1015517471@qq.com>
2016-08-01 14:53:00 +0800

    test

--- app/Http/Middleware/EmailMiddleware.php
+++ app/Http/Middleware/EmailMiddleware.php
@@ -42,24 +42,6 @@ class EmailMiddleware
             'secure' => 'ssl',
         ]);
         $mailer->send($mail);
-
-//        $mails = "1015517471@qq.com";
-//        $mail = new Message;
-//        $mail->setFrom( env('SMTP_NAME') .' <'.env('SMTP_USERNAME').'>')
-//            ->addTo($mails)
-//            ->setSubject('来自宁夏E营销的确认邮件!')
-//            ->setHTMLBody("
-//            <h3>黄河 : 您好,这是一封确认邮件!</h3><br>
-//            <a href='http://".$_SERVER['SERVER_NAME']."/test/mail/sdfsd' target='_blank'>请点击进行邮箱认证!</a>
-//                ");
-//        $mailer = new SmtpMailer([
-//            'host' => env('SMTP_HOST'),
-//            'username' => env('SMTP_USERNAME'),
-//            'password' => env('SMTP_PWD'),
-//            'secure' => 'ssl',
-//        ]);
-//        $mailer->send($mail);
-//        echo "测试一下 事实上";
         return $rs;
     }
 }
