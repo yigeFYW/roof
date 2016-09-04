a12d7cbf4e2e3004963e4a7167f1a25d17733fe7
yigeFYW <1015517471@qq.com>
2016-09-04 09:29:57 +0800

    test

--- database/migrations/2016_09_03_050745_add_rep_to_accounts.php
+++ database/migrations/2016_09_03_050745_add_rep_to_accounts.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddRepToAccount extends Migration
+class AddRepToAccounts extends Migration
 {
     /**
      * Run the migrations.
